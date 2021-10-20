f1ab92d6d64180705493537cfb375830ee2f593e
BaLuisTo <bryan.luis@segurosbolivar.com>
2021-10-20 16:03:14 -0500

    Se agrega modelo de hoja historia

---
+++ models/medicina.hojaHistoria.go
@@ -0,0 +1,152 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/astaxie/beego/orm"
+)
+
+type MedicinaHojaHistoria struct {
+	IdHojaHistoria    int       `orm:"column(id_hoja_historia);pk;auto"`
+	FechaConsulta     time.Time `orm:"column(fecha_consulta);type(date);null"`
+	Motivo            string    `orm:"column(motivo);null"`
+	IdDiagnostico     int       `orm:"column(id_diagnostico);null"`
+	Observacion       string    `orm:"column(observacion);null"`
+	Evolucion         string    `orm:"column(evolucion);null"`
+	IdEspecialidad    int       `orm:"column(id_especialidad);null"`
+	IdProfesional     int       `orm:"column(id_profesional);null"`
+	IdHistoriaClinica int       `orm:"column(id_historia_clinica);null"`
+	IdPersona         int       `orm:"column(id_persona);null"`
+}
+
+func (t *MedicinaHojaHistoria) TableName() string {
+	return "hojahistoria"
+}
+func init() {
+	orm.RegisterModel(new(MedicinaHojaHistoria))
+}
+
+// AddMedicinaHojaHistoria inserta un registro en la tabla hoja_historia
+//Último registro insertado con éxito
+func AddMedicinaHojaHistoria(m *MedicinaHojaHistoria) (err error) {
+	o := orm.NewOrm()
+	m.IdHojaHistoria = 0
+	if _, err = o.Insert(m); err == nil {
+		return nil
+	}
+	return err
+}
+
+//GetMedicinaHojaHistoriaById trae un registro de la tabla hoja_historia
+//Id no existe
+func GetMedicinaHojaHistoriaById(id int) (v *MedicinaHojaHistoria, err error) {
+	o := orm.NewOrm()
+	v = &MedicinaHojaHistoria{IdHojaHistoria: id}
+	if err = o.Read(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}
+
+//GetAllMedicinaHojaHistoria trae todos los registros de la tabla hoja_historia
+//No existen registros
+func GetAllMedicinaHojaHistoria(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (ml []interface{}, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable(new(MedicinaHojaHistoria))
+	for k, v := range query {
+		k = strings.Replace(k, ".", "__", -1)
+		if strings.Contains(k, "isnull") {
+			qs = qs.Filter(k, (v == "true" || v == "1"))
+		} else {
+			qs = qs.Filter(k, v)
+		}
+	}
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Orden inválido, debe ser del tipo [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Orden inválido, debe ser del tipo [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: los tamaños de 'sortby', 'order' no coinciden o el tamaño de 'order' no es 1")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: campos de 'order' no utilizados")
+		}
+	}
+	var l []MedicinaHojaHistoria
+	qs = qs.OrderBy(sortFields...)
+	if _, err = qs.Limit(limit, offset).All(&l, fields...); err == nil {
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		return ml, nil
+	}
+	return nil, err
+}
+
+//UpdateMedicinaHojaHistoria actualiza un registro en la tabla hoja_historia
+//El registro a actualizar no existe
+func UpdateMedicinaHojaHistoria(m *MedicinaHojaHistoria) (err error) {
+	o := orm.NewOrm()
+	v := MedicinaHojaHistoria{IdHojaHistoria: m.IdHojaHistoria}
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Update(m); err == nil {
+			fmt.Println("Numero de registros actualizados:", num)
+		}
+	}
+	return
+}
+
+//DeleteMedicinaHojaHistoria elimina un registro de la tabla hoja_historia
+//EL registro a eliminar no existe
+func DeleteMedicinaHojaHistoria(id int) (err error) {
+	o := orm.NewOrm()
+	v := MedicinaHojaHistoria{IdHojaHistoria: id}
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Delete(&MedicinaHojaHistoria{IdHojaHistoria: id}); err == nil {
+			fmt.Println("Numero de registros eliminados:", num)
+		}
+	}
+	return
+}
